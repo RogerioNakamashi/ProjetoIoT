3a987c2f103a1eae2e1a0d77c711ccf2dadc6f0a
RogerioNakamashi <rogerionakamashi@gmail.com>
2019-07-24 15:06:16 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+import { Text } from 'react-native';
+import NewAlarm from './screens.js/new_alarm.page';
+
+const App = () => {
+  return (
+    <NewAlarm/>
+  );
+};
+
+export default App;
