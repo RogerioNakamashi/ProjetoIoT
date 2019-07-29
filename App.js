a8b186fa3d59bb8a8ee4abc0be3a73308c2c7950
RogerioNakamashi <rogerionakamashi@gmail.com>
2019-07-29 10:43:52 -0300

    add floating action button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
 import { Text } from 'react-native';
-import NewAlarm from './screens.js/new_alarm.page';
+import NewAlarm from './screens/new_alarm.page';
 
 const App = () => {
   return (
